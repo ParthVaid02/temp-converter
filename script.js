7cafad97f089458ad58dad62e8eb8c322a8c1743
ParthVaid02 <vaidparth02@gmail.com>
2025-05-12 06:22:59 +0530

    make temp converter

---
+++ script.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form');
+const result = document.querySelector('#result')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const temp = parseInt(document.querySelector('#temp').value);
+    if(document.querySelector('#toFahrenhiet').checked){
+        const convertedTemp = ((temp*(9/5)) + 32).toFixed(2);
+        result.innerHTML = `Your result is ${convertedTemp} °F`;
+    }
+    else{
+        const convertedTemp = ((5/9)*(temp - 32)).toFixed(2);
+        result.innerHTML = `Your result is ${convertedTemp} °C`;
+    }
+})
